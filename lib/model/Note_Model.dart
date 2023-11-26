4a05cd2164b5e8c56d4953a6cfed65b1c3b4b469
Tarek Rashad <Tarek Rashad>
2023-11-26 21:42:31 +0200

    create note model

---
+++ lib/model/Note_Model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final DateTime date;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+  });
+}
