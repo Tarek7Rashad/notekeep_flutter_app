58f9b6c9a180b6eaded69b546363d0a0aeae13bb
Tarek Rashad <Tarek Rashad>
2023-11-27 11:58:50 +0200

    add cubit provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notekeep_flutter_app/Screen/Note_EditingScreen.dart';
 import 'package:notekeep_flutter_app/Screen/Note_HomeScreen.dart';
+import 'package:notekeep_flutter_app/cubits/AddNoteCubit/cubit/add_note_cubit.dart';
 import 'package:notekeep_flutter_app/model/Note_Model.dart';
 import 'package:notekeep_flutter_app/shared/components/constant.dart';
 
@@ -17,16 +19,21 @@ class NoteKeepApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: NoteKeepHomeScreen.id,
-      routes: {
-        NoteKeepHomeScreen.id: (context) => const NoteKeepHomeScreen(),
-        NoteEditing.id: (context) => const NoteEditing(),
-      },
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Lato",
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: NoteKeepHomeScreen.id,
+        routes: {
+          NoteKeepHomeScreen.id: (context) => const NoteKeepHomeScreen(),
+          NoteEditing.id: (context) => const NoteEditing(),
+        },
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: "Lato",
+        ),
       ),
     );
   }
