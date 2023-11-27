a531d4bdd9c722296ccc4979add6fdb8242d2db0
Tarek Rashad <Tarek Rashad>
2023-11-27 09:21:18 +0200

    generate Note Model using hive generator

--- lib/model/Note_Model.dart
+++ lib/model/Note_Model.dart
@@ -1,6 +1,12 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'Note_Model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject  {
+  @HiveField(0)
   final String title;
+    @HiveField(1)
   final String subTitle;
+    @HiveField(2)
   final DateTime date;
 
   NoteModel({
