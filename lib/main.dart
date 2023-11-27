f6ccaa7a3b9d7a6ce137be41e30c638d9b8df579
Tarek Rashad <Tarek Rashad>
2023-11-27 09:29:24 +0200

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notekeep_flutter_app/Screen/Note_EditingScreen.dart';
 import 'package:notekeep_flutter_app/Screen/Note_HomeScreen.dart';
+import 'package:notekeep_flutter_app/shared/components/constant.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
   runApp(const NoteKeepApp());
 }
 
--- lib/shared/components/constant.dart
+++ lib/shared/components/constant.dart
@@ -4,3 +4,4 @@ const kMattWhiteColor = Color(0xffdbddda);
 const kLightBlackColor = Color(0xff555555);
 const kDarkPurpleColor = Color(0xff443C68);
 const kMoreDarkPurpleColor = Color(0xff393053);
+const kNoteBox = "Note_Model";
