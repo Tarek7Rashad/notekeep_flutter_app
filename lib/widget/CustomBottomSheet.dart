a3852c1cdf399446f3b27f4e91d70d277adc3309
Tarek Rashad <Tarek Rashad>
2023-11-28 10:24:09 +0200

    create addnote at bottomsheet

--- lib/widget/CustomBottomSheet.dart
+++ lib/widget/CustomBottomSheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:notekeep_flutter_app/widget/CustomAddButton.dart';
-import 'package:notekeep_flutter_app/widget/CustomTextFormField.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notekeep_flutter_app/cubits/AddNoteCubit/cubit/add_note_cubit.dart';
+import 'package:notekeep_flutter_app/widget/FormOfAddNote.dart';
 
 class CustomBottomSheet extends StatelessWidget {
   const CustomBottomSheet({
@@ -9,79 +11,25 @@ class CustomBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 30, right: 20, left: 20, bottom: 10),
+    return Padding(
+      padding: const EdgeInsets.only(top: 30, right: 20, left: 20, bottom: 10),
       child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: FieldsOfAddNewNote(),
-      ),
-    );
-  }
-}
-
-class FieldsOfAddNewNote extends StatefulWidget {
-  const FieldsOfAddNewNote({
-    super.key,
-  });
-
-  @override
-  State<FieldsOfAddNewNote> createState() => _fieldsOfAddNewNoteState();
-}
-
-final GlobalKey<FormState> formKey = GlobalKey();
-AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-String? title;
-String? subTitle;
-
-// ignore: camel_case_types
-class _fieldsOfAddNewNoteState extends State<FieldsOfAddNewNote> {
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          CustomTextFormField(
-            onSaved: (input) {
-              title = input;
-            },
-            hint: "Note Title",
-            label: "Note Title",
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomTextFormField(
-            onSaved: (input) {
-              subTitle = input;
-            },
-            maxLines: 5,
-            hint: "Note",
-            label: "Note",
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomAddButton(
-            text: "Add",
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
+          physics: const BouncingScrollPhysics(),
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              if (state is AddNoteFailure) {
+                print('success');
+              }
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
               }
             },
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+            builder: (context, state) {
+              return ModalProgressHUD(
+                  inAsyncCall: state is AddNoteLoading ? true : false,
+                  child: const FieldsOfAddNewNote());
+            },
+          )),
     );
   }
 }
---
+++ lib/widget/FormOfAddNote.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:notekeep_flutter_app/widget/CustomAddButton.dart';
+import 'package:notekeep_flutter_app/widget/CustomTextFormField.dart';
+
+class FieldsOfAddNewNote extends StatefulWidget {
+  const FieldsOfAddNewNote({
+    super.key,
+  });
+
+  @override
+  State<FieldsOfAddNewNote> createState() => _fieldsOfAddNewNoteState();
+}
+
+final GlobalKey<FormState> formKey = GlobalKey();
+AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+String? title;
+String? subTitle;
+
+// ignore: camel_case_types
+class _fieldsOfAddNewNoteState extends State<FieldsOfAddNewNote> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          CustomTextFormField(
+            onSaved: (input) {
+              title = input;
+            },
+            hint: "Note Title",
+            label: "Note Title",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomTextFormField(
+            onSaved: (input) {
+              subTitle = input;
+            },
+            maxLines: 5,
+            hint: "Note",
+            label: "Note",
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomAddButton(
+            text: "Add",
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
